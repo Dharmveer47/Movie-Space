1479568ed26078d0c6cb13c187349002f05db848
dharmveer47 <singhdharmu89@gmail.com>
2024-02-26 17:15:08 +0530

    fixed: image bug

--- src/Api.js
+++ src/Api.js
@@ -1,35 +1,33 @@
-
 // Configuration for TMDB API
 // Read more about the API here: https://developers.themoviedb.org/
 
-const API_URL = 'https://api.themoviedb.org/3/';
+const API_URL = "https://api.themoviedb.org/3/";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 // console.log(API_KEY2)
 // Tranding Movies and TvShow
 export const MEDIA_TYPE = {
-  ALL: 'all',
-  MOVIE: 'movie',
-  TV: 'tv',
-  PERSON: 'person'
-}
+  ALL: "all",
+  MOVIE: "movie",
+  TV: "tv",
+  PERSON: "person",
+};
 export const TIME_WINDIW = {
-  DAY : 'day',
-  WEEK : 'week'
-}
-export const TRENDING_MOVIES = `${API_URL}trending/${MEDIA_TYPE.MOVIE}/${TIME_WINDIW.DAY}?api_key=${API_KEY}&language=en-US&page=`
-export const TRENDING_TVSHOW = `${API_URL}trending/${MEDIA_TYPE.TV}/${TIME_WINDIW.DAY}?api_key=${API_KEY}&language=en-US&page=`
-//Latest Movies 
-const LATEST_MOVIES = `${API_URL}movie/latest?api_key=${API_KEY}&language=en-US&page=`
+  DAY: "day",
+  WEEK: "week",
+};
+export const TRENDING_MOVIES = `${API_URL}trending/${MEDIA_TYPE.MOVIE}/${TIME_WINDIW.DAY}?api_key=${API_KEY}&language=en-US&page=`;
+export const TRENDING_TVSHOW = `${API_URL}trending/${MEDIA_TYPE.TV}/${TIME_WINDIW.DAY}?api_key=${API_KEY}&language=en-US&page=`;
+//Latest Movies
+const LATEST_MOVIES = `${API_URL}movie/latest?api_key=${API_KEY}&language=en-US&page=`;
 //Upcoming Movies
-export const TOP_RATED = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&page=`
-const Upcoming_Movies = `${API_URL}movie/upcoming?api_key=${API_KEY}&language=en-US&page=`
+export const TOP_RATED = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&page=`;
+const Upcoming_Movies = `${API_URL}movie/upcoming?api_key=${API_KEY}&language=en-US&page=`;
 const SEARCH_BASE_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=`;
 const POPULAR_BASE_URL = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=`;
 
-
-export const LATEST_TV = `${API_URL}tv/latest?api_key=${API_KEY}&language=en-US&page=`
-export const Upcoming_TvShow = `${API_URL}tv/upcoming?api_key=${API_KEY}&language=en-US&page=`
+export const LATEST_TV = `${API_URL}tv/latest?api_key=${API_KEY}&language=en-US&page=`;
+export const Upcoming_TvShow = `${API_URL}tv/upcoming?api_key=${API_KEY}&language=en-US&page=`;
 export const POPULAR_BASE_URL_Tv = `${API_URL}tv/popular?api_key=${API_KEY}&language=en-US&page=`;
 
 // For login and voting
@@ -38,19 +36,17 @@ const LOGIN_URL = `${API_URL}authentication/token/validate_with_login?api_key=${
 
 const SESSION_ID_URL = `${API_URL}authentication/session/new?api_key=${API_KEY}`;
 
-const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/w154';
-export const IMAGE_BASE_URL_CARD = 'https://www.themoviedb.org/t/p/w220_and_h330_face';
+const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/w154";
+export const IMAGE_BASE_URL_CARD = "https://image.tmdb.org/t/p/w500";
 // Sizes: w300, w780, w1280, original
-const BACKDROP_SIZE = 'w1280';
+const BACKDROP_SIZE = "w1280";
 // w92, w154, w185, w342, w500, w780, original
-const POSTER_SIZE = 'w780';
-const CARD_SIZE = 'w500';
+const POSTER_SIZE = "w780";
+const CARD_SIZE = "w500";
 const VIDEO_PALY = `https://www.themoviedb.org/video/play?key=`;
 
 //-------------- Config for TvShow --------------------
-const POPULAR_TVSHOW = `${API_URL}tv/popular?api_key=${API_KEY}&language=en-US&page=`
-
-
+const POPULAR_TVSHOW = `${API_URL}tv/popular?api_key=${API_KEY}&language=en-US&page=`;
 
 export {
   POPULAR_TVSHOW,
--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,6 @@ import { NextPrevious } from "./MovieFilter";
 import Poster from "../img/ProfileDrawer.png";
 
 const Card = ({ showType = "initial", type, search, cast = [] }) => {
-  // console.log(cast);
   const [MoviInfo, setMoviInfo] = useState([]);
   const [total_pages, settotal_pages] = useState([]);
   const [next, setNext] = useState(1);
@@ -40,8 +39,8 @@ const Card = ({ showType = "initial", type, search, cast = [] }) => {
 
   useEffect(() => {
     fetchData(showUrl);
-    
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [showUrl]);
 
   const Loading = () => {
@@ -49,8 +48,8 @@ const Card = ({ showType = "initial", type, search, cast = [] }) => {
       <div className="h-52 relative m-2 my-3 sha dow-lg  rounded-xl bg-gradient-to-r from-purple-500 to-pink-500 animate-pulse border-4  min-w-[200px]"></div>
     );
   };
-  console.log(MoviInfo)
-  console.log(cast)
+  console.log(MoviInfo);
+  console.log(cast);
 
   if (MoviInfo.length === 0 || MoviInfo === "undefined") {
     return (
@@ -76,6 +75,8 @@ const Card = ({ showType = "initial", type, search, cast = [] }) => {
     data.push(MoviInfo.slice(i, i + size));
   }
 
+  console.log(data, "single_card");
+
   return (
     <>
       <div className="flexCB z-10 w-[90%] mx-auto  overflow-scroll overflow-y-hidden ">
@@ -98,55 +99,62 @@ const SingleCard = ({ data, type }) => {
     return <div></div>;
   }
   return (
-    <>
-      <div className="pb-8" onClick={() => window.scrollTo(0, 0)}>
-        <div className="m-3 my-5">
-          <Link
-            to={`/MoviesExple/${
-              data[0].media_type ? data[0].media_type : type
-            }/${data[0].id}`}
-          >
-            <SingleCard01 data={data[0]} />
-          </Link>
-        </div>
-        <div className="m-3 my-5">
-          <Link
-            to={`/MoviesExple/${
-              data[1].media_type ? data[1].media_type : type
-            }/${data[1].id} `}
-          >
-            <SingleCard02 data={data[1]} />
-          </Link>
-        </div>
+    <div className="pb-8" onClick={() => window.scrollTo(0, 0)}>
+      <div className="m-3 my-5">
+        <Link
+          to={`/MoviesExple/${data[0].media_type ? data[0].media_type : type}/${
+            data[0].id
+          }`}
+        >
+          <SingleCard01 data={data[0]} />
+        </Link>
       </div>
-    </>
+      <div className="m-3 my-5">
+        <Link
+          to={`/MoviesExple/${data[1].media_type ? data[1].media_type : type}/${
+            data[1].id
+          } `}
+        >
+          <SingleCard02 data={data[1]} />
+        </Link>
+      </div>
+    </div>
   );
 };
 
 const SingleCard01 = ({ data }) => {
-  let cardImg = `${IMAGE_BASE_URL_CARD}${data.poster_path}`;
+  const [imageError, setImageError] = useState(false);
+  const cardImg = `${IMAGE_BASE_URL_CARD}${data.poster_path}`;
+  const defaultImage = Poster; // Assuming Poster is defined somewhere in your code
+  console.log(cardImg);
+  const handleImageError = () => {
+    setImageError(true);
+  };
+
   return (
-    <div className="relative ">
+    <div className="relative">
       <img
-        loading="lazy"
-        src={`${cardImg === null ? Poster : cardImg}`}
+        src={imageError ? defaultImage : cardImg}
         alt=""
-        className="mdx:min-w-[100px]  h-auto min-w-[160px]   rounded-md shadow-lg transition duration-500"
+        onError={handleImageError}
+        className="mdx:min-w-[100px] h-auto min-w-[160px] rounded-md shadow-lg transition duration-500"
       />
-      <div className=" w-8 h-8 text-xs rounded-full bg-gray-900 flexCC text-white font-bold border-gray-50 border-2 absolute     -bottom-3 -right-3 ">
+      <div className="w-8 h-8 text-xs rounded-full bg-gray-900 flexCC text-white font-bold border-gray-50 border-2 absolute -bottom-3 -right-3">
         {data.vote_average.toString().slice(0, 3)}
       </div>
     </div>
   );
 };
 const SingleCard02 = ({ data }) => {
-  let cardImg = `${IMAGE_BASE_URL_CARD}${data.poster_path}`;
+  const cardImg = `${IMAGE_BASE_URL_CARD}${data.poster_path}`;
+
   return (
     <div className="relative">
       <img
-        src={`${cardImg === null ? Poster : cardImg}`}
+        className="mdx:min-w-[100px]  h-auto min-w-[160px]   rounded-md shadow-lg transition duration-500"
+        loading="lazy"
+        src={cardImg}
         alt=""
-        className="mdx:min-w-[100px] h-auto min-w-[160px]  rounded-md shadow-lg"
       />
       <div className=" w-8 h-8 text-xs rounded-full bg-gray-900 flexCC text-white font-bold border-gray-50 border-2 absolute     -bottom-3 -right-3 ">
         {data.vote_average.toString().slice(0, 3)}
